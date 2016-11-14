65b537e04ebc1bb9d6b7833e4fc6da5d451ce377
ssalin <ssalin@pdx.edu>
2016-11-13 22:28:12 -0800

    more struture

--- HW2/hw2.py
+++ HW2/hw2.py
@@ -41,10 +41,10 @@ def dlight (FREQUENCY, LENGTH):
 
 #See http://www.phy.mtu.edu/~suits/notefreqs.html
 #F = [261.63, 440, 293.66] #Hz, waves per second, 261.63=C4-note.
-F = [0, 1, 2, 3, 4]#, 4, 3, 2, 1, 0, 3] #10 items
-L = 5#[5, 5, 5, 5, 5] #.5, .6, .7, .8, .9, 1] #seconds to play sound
+#F = [0, 1, 2, 3, 4]#, 4, 3, 2, 1, 0, 3] #10 items
+#L = 5#[5, 5, 5, 5, 5] #.5, .6, .7, .8, .9, 1] #seconds to play sound
 
-def crossover(parent1, parent2, child1, child2);
+def crossover(parent1, parent2, child1, child2):
     #since we need to change the globals
     global P1
     global P2
@@ -64,37 +64,61 @@ def crossover(parent1, parent2, child1, child2);
 
 #def evaluate ()
 #   "determines fitness of genetics"
+#   ask user "first or second?"
+#   get user response
+#   P1 = P2
+#   if ( user liked C1
+        P2 = C1
+    elif
+        P2 = C2
+        
 #   return
 
+
+
+def play(F):
 #while loop to continue producing lightshows
 # 10 steps in sequence
 # length can be from 0 - 1 second
 #0-5 for freq, 0 being light 1 4 being light 5. 5 is off
-
-    # loop to play a sequence
 for i in range(len(F)):
-
-        #LENGTH = L[i]
-        #grab frequency based on #
-        #if block to convert 0-4 into a specifc freq
+        #if block to convert 0-5 into a specifc freq
         wait = 1;
-        if F[i] == 0:
+        if F[i] == 0: #Light 1
             FREQUENCY = 25 #261.63
-        elif F[i] == 1:
+        elif F[i] == 1: #Light 2
             FREQUENCY = 300 #293.66
-        elif F[i] == 2:
+        elif F[i] == 2:#Light 3
             FREQUENCY = 600
-        elif F[i] == 3:
+        elif F[i] == 3:#Light 4
             FREQUENCY = 1500
-        elif F[i] == 4:
+        elif F[i] == 4:#Light 5
             FREQUENCY = 4000
-        elif F[i] == 5:
+        elif F[i] == 5:#Lights Off
             wait = 0
             
         if(wait)
             dlight(FREQUENCY, L)
-        if(!wait)
+        elif(!wait)
             time.sleep(L)
+#end of loop
+return
+
+def main():
+    # loop to play a sequence
+    C1 = P1
+    C2 = P2
+  #DO
+    play(C1)
+    play(C2)
+    #evaluate
+    crossover()
+    
+  #while(yes)
     #end loop
+
 #evaluate();
 #again?
+            
+if __name__=="__main__":
+    main()
