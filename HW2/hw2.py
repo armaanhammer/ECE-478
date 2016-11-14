22ee59eac88b14b9d78751adece4d98c25786691
ssalin <ssalin@pdx.edu>
2016-11-13 21:46:27 -0800

    added crossover, global chromosomes

--- HW2/hw2.py
+++ HW2/hw2.py
@@ -5,7 +5,13 @@ import pyaudio
 PyAudio = pyaudio.PyAudio
 
 #See http://en.wikipedia.org/wiki/Bit_rate#Audio
-BITRATE = 16000 #number of frames per second/frameset.      
+BITRATE = 16000 #number of frames per second/frameset.
+
+#Global Chromosomes
+    P1 = [0,1,2,3,4,5,4,3,2,1] #Parent 1
+    P2 = [0,1,2,3,4,5,5,5,5,5] #Parent 2
+    C1 = [0,0,0,0,0,0,0,0,0,0] #Child 1
+    C2 = [0,0,0,0,0,0,0,0,0,0] #Child 2
 
 def dlight (FREQUENCY, LENGTH):
     "plays a frequency for a length"
@@ -32,21 +38,23 @@ def dlight (FREQUENCY, LENGTH):
     p.terminate()
     return
 
-def crossover(parent1_f, parent2_f, child1_f, child2_f);
+#See http://www.phy.mtu.edu/~suits/notefreqs.html
+#F = [261.63, 440, 293.66] #Hz, waves per second, 261.63=C4-note.
+F = [0, 1, 2, 3, 4]#, 4, 3, 2, 1, 0, 3] #10 items
+L = 5#[5, 5, 5, 5, 5] #.5, .6, .7, .8, .9, 1] #seconds to play sound
+
+def crossover(parent1, parent2, child1, child2);
     #   "crossover function"
     index1 = random.randint(1, len(parent1_l) - 2)
     index2 = random.randint(1, len(parent2_l) - 2):
-    parent1_f = [3, 2, 5, 1, 0]
-    parent2_f = [4, 3, 2, 1, 0]
-        for i in range(len(parent1_l)):
-            child1_f[i] = parent1_f[:index1] + parent2_f[index1:]
-            child2_f[i] = parent1_f[:index2] + parent2_f[index2:]
+    parent1 = [3, 2, 5, 1, 0]
+    parent2 = [4, 3, 2, 1, 0]
+    for i in range(len(parent1_l)):
+        child1[i] = parent1[:index1] + parent2[index1:]
+        child2[i] = parent1[:index2] + parent2[index2:]
                     
-            #end for loop
-        return (child1_f, child2_f)
-
-
-
+    #end for loop
+    return (child1, child2)
 
 #def evaluate ()
 #   "determines fitness of genetics"
@@ -57,14 +65,8 @@ def crossover(parent1_f, parent2_f, child1_f, child2_f);
 # length can be from 0 - 1 second
 #0-4 for freq, 0 being light 1 4 being light 5
 
-def main():
-#See http://www.phy.mtu.edu/~suits/notefreqs.html
-#F = [261.63, 440, 293.66] #Hz, waves per second, 261.63=C4-note.
-    F = [0, 1, 2, 3, 4]#, 4, 3, 2, 1, 0, 3] #10 items
-    L = 5#[5, 5, 5, 5, 5] #.5, .6, .7, .8, .9, 1] #seconds to play sound
-
     # loop to play a sequence
-    for i in range(len(F)):
+for i in range(len(F)):
 
         #LENGTH = L[i]
         #grab frequency based on #
@@ -84,6 +86,3 @@ def main():
     #end loop
 #evaluate();
 #again?
-
-if __name__=="__main__":
-    main()
