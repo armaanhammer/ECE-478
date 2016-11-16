4a505f1893fd8372fdac5fe5bc1366e6f8c19580
Salyhakkoum <shakkoum@pdx.edu>
2016-11-16 15:35:28 -0800

    New revision of crossover

---
+++ HW2/HW#2_Saly's revision.py
@@ -0,0 +1,40 @@
+mport random
+
+def main():
+    p1 = [0,1,2,3,4,5,4,3,2,1] #Parent 1
+    p2 = [0,1,2,3,4,5,5,5,5,5] #Parent 2
+    
+    p1,p2 = crossover(p1,p2)
+    
+    print "This is new list for parent 1 {}".format(p1);
+    print "This is new list for parent 2 {}".format(p2);
+
+def crossover(parent1, parent2):
+    c1 = []
+    c2 = []
+    size = len(parent1)
+    index1 = random.randrange(0,size)
+    print index1
+    index2 = random.randrange(0,size)
+    print index2
+    if (index1 < index2) or (index1 == index2):
+        temp1 = parent1[0:index1]
+        temp2 = parent1[index1:size]
+        c1 = temp2 + temp1
+        temp1 = parent2[0:index2]
+        temp2 = parent2[index2:size]
+        c2 = temp2 + temp1
+    else:
+        temp1 = parent1[0:index2]
+        temp2 = parent1[index2:size]
+        c1 = temp2 + temp1
+        temp1 = parent2[0:index1]
+        temp2 = parent2[index1:size]
+        c2 = temp2 + temp1
+    
+    return c1,c2
+
+if __name__=="__main__":
+    main()
+
+
