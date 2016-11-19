af7fe917ffddc9fe1c9c2f4fc2fd966c3de0a671
ssalin <ssalin@pdx.edu>
2016-11-18 17:12:19 -0800

    updates

--- HW2/hw_2.py
+++ HW2/hw_2.py
@@ -11,11 +11,11 @@ PyAudio = pyaudio.PyAudio
 BITRATE = 16000 #number of frames per second/frameset.
 
 #Global Chromosomes. all have 10 Alleles
-P1 = [0,1,2,3,4,5,4,3,2,1] #Parent 1
-P2 = [0,1,2,3,4,5,5,5,5,5] #Parent 2
-C1 = [0,1,2,3,4,5,4,3,2,1] #Child 1
-C2 = [0,1,2,3,4,5,5,5,5,5] #Child 2
-L = 1
+P1 = [0,1,2,3,4,5,4,3,2,1] # Parent 1
+P2 = [0,1,2,3,4,5,5,5,5,5] # Parent 2
+C1 = [0,1,2,3,4,5,4,3,2,1] # Child 1
+C2 = [0,1,2,3,4,5,5,5,5,5] # Child 2
+L = 1 # length to play a sound for
     
 def dlight (FREQUENCY, LENGTH):
     "plays a frequency for a length"
@@ -44,37 +44,64 @@ def dlight (FREQUENCY, LENGTH):
 
 def miccheck():
     "loop to find the light frequencies"
-    freq = 4000;
-    for i in range(0,40):
+    freq = 4000; # starting frequency
+    for i in range(0,40): # band to  sweep
         print ('freq = ', freq)
-        dlight(freq, 5)
-        freq = freq + 100
-    #end for loop
+        dlight(freq, 5)   # see what lights get triggered
+        freq = freq + 100 # increment size
+    # end for loop
     return
 
 def crossover():
     "make children"
-    #since we need to change the globals
+    # since we need to change the globals
     global C1
     global C2
-    #   "crossover function"
     size = len(P1)
-    index = random.randrange(0,size)
-    temp1 = P1[0:index]
-    temp2 = P1[index:size]
-    C1 = temp2 + temp1
-    temp1 = P2[0:index]
-    temp2 = P2[index:size]
-    C2 = temp2 + temp1
-                    
-    #end for loop
+    index1 = random.randint(1, size - 1)
+    #print index1
+    index2 = random.randint(1, size - 1)
+    #print index2
+    if index1 > index2:
+        index1 = index2
+        index2 = index1
+    temp1 = P1[0:index1]
+    temp2 = P2[index1:index2]
+    temp3 = P1[index2:size]
+    C1 = temp1 + temp2 + temp3
+    temp1 = P2[0:index1]
+    temp2 = P1[index1:index2]
+    temp3 = P2[index2:size]
+    C2 = temp1 + temp2 + temp3
+    mutate_one_index(C1)
+    mutate_mult_index(C2)
     return
+
+def mutate_one_index(C):
+    size = len(C)
+    index = random.randrange(0,size)
+    mutate_value = random.randrange(0,5)
+    C1[index] = mutate_value 
+    return 
+
+def mutate_mult_index(C):
+    size = len(C)
+    while True:
+        index1 = random.randrange(0,size)
+        index2 = random.randrange(0,size)
+        if (index1 != index2):
+           temp  = C[index1]
+           C[index1] = C[index2]
+           C[index2] = temp
+           break
+    
+    return 
 
 def evaluate():
    "select children for crossover"
    global P1
    global P2
-   P1 = P2 #get rid of oldest parent
+   P1 = P2 # get rid of oldest parent
 #   ask user "first or second?"
    while 1:    
     x = int(input("Press 1 if you like chromose 1 , Press 2 if you like chromosome 2"))
@@ -86,28 +113,29 @@ def evaluate():
         print("Chromosome 2 selected")
         P2 = C2
         break
+    elif x==3:
+        print("neither was adequate")
+        break
     else:
         print("wrong selection, please press  1 or 2")
 
 def play(F):
     "play sequence"
-# 10 steps in sequence
-# length can be from 0 - 1 second
 #0-5 for freq, 0 being light 1 4 being light 5. 5 is off
     FREQUENCY = 1
     for i in range(len(F)):
         wait = 1;
-        if F[i] == 0: #Light 1
+        if F[i] == 0:  # Light 1
             FREQUENCY = 30 #25 #261.63
-        elif F[i] == 1: #Light 2
+        elif F[i] == 1:# Light 2
             FREQUENCY = 300 #293.66
-        elif F[i] == 2:#Light 3
+        elif F[i] == 2:# Light 3
             FREQUENCY = 700#600
-        elif F[i] == 3:#Light 4
+        elif F[i] == 3:# Light 4
             FREQUENCY = 1500
-        elif F[i] == 4:#Light 5
+        elif F[i] == 4:# Light 5
             FREQUENCY = 6000
-        elif F[i] == 5:#Lights Off
+        elif F[i] == 5:# Lights Off
             wait = 0 
         if wait == 1:
             dlight(FREQUENCY, L)
@@ -115,9 +143,8 @@ def play(F):
             time.sleep(L)
 
 def main():
-    #Seed Parents?
     #first children
-    gen = 0
+    gen = 0 # generation counter
     again = 1
    # miccheck()
     while(again):
